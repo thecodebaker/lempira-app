27189e933d5697d8033b246f96a183f994033803
David Mendoza <d.mendoza99@unitec.edu>
2021-03-25 18:16:08 -0600

    allow js y tambien reescritura completa del app para poder manejarlo como queria hasta los color scheme

--- App.tsx
+++ App.tsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { ThemeProvider } from "react-native-elements";
-import { useColorScheme, AppearanceProvider } from "react-native-appearance";
-import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { Provider, useSelector, RootStateOrAny } from "react-redux";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { PersistGate } from "redux-persist/integration/react";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import { getStore, getPersistor } from "./src/redux/store";
-import { LightTheme, DarkTheme } from "./src/themes";
-import Movements from "./src/views/Movements";
-import Login from "./src/views/Login";
-import Settings from "./src/views/Settings";
-import Signup from "./src/views/Signup";
-import Stats from "./src/views/Stats";
-import Accounts from "./src/views/Accounts";
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ThemeProvider } from 'react-native-elements';
+import { useColorScheme, AppearanceProvider } from 'react-native-appearance';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { Provider, useSelector, RootStateOrAny } from 'react-redux';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { PersistGate } from 'redux-persist/integration/react';
+import { LightTheme, DarkTheme } from './src/themes';
+import { getStore, getPersistor } from './src/redux/store';
+import Movements from './src/views/Movements';
+import Login from './src/views/Login';
+import Settings from './src/views/Settings';
+import Signup from './src/views/Signup';
+import Stats from './src/views/Stats';
+import AccountNavigator from './src/Navigators/AccountNavigator';
+import User from './src/Types/User';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -31,14 +32,14 @@ const AppContainer = () => {
         <PersistGate persistor={myPersistor}>
           <StatusBar
             style="auto"
-            backgroundColor={colorScheme === "dark" ? "black" : "white"}
+            backgroundColor={colorScheme === 'dark' ? 'black' : 'white'}
           />
           <Provider store={myStore}>
             <ThemeProvider
-              theme={colorScheme === "dark" ? DarkTheme : LightTheme}
+              theme={colorScheme === 'dark' ? DarkTheme : LightTheme}
             >
               <NavigationContainer
-                theme={colorScheme === "dark" ? DarkTheme : LightTheme}
+                theme={colorScheme === 'dark' ? DarkTheme : LightTheme}
               >
                 <SafeAreaView
                   style={{
@@ -57,7 +58,7 @@ const AppContainer = () => {
 };
 
 const App = () => {
-  const user = useSelector((state: RootStateOrAny) => state.authReducer.user);
+  const user: User = useSelector((state: RootStateOrAny) => state.auth.user);
   const colorScheme = useColorScheme();
   if (user.token === undefined)
     return (
@@ -75,29 +76,29 @@ const App = () => {
       <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
-            let iconName: string = "";
-            if (route.name === "Movimientos") {
+            let iconName: string = '';
+            if (route.name === 'Movimientos') {
               iconName = focused
-                ? "swap-horizontal"
-                : "swap-horizontal-outline";
-            } else if (route.name === "Ajustes") {
-              iconName = focused ? "settings" : "settings-outline";
-            } else if (route.name === "Estadisticas") {
-              iconName = focused ? "analytics" : "analytics-outline";
-            } else if (route.name === "Cuentas") {
-              iconName = focused ? "file-tray-full" : "file-tray-full-outline";
+                ? 'swap-horizontal'
+                : 'swap-horizontal-outline';
+            } else if (route.name === 'Ajustes') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            } else if (route.name === 'Estadisticas') {
+              iconName = focused ? 'analytics' : 'analytics-outline';
+            } else if (route.name === 'Cuentas') {
+              iconName = focused ? 'file-tray-full' : 'file-tray-full-outline';
             }
             return <Ionicons name={iconName} size={size} color={color} />;
           },
         })}
         tabBarOptions={{
-          activeTintColor: "#34AF4A",
-          inactiveTintColor: colorScheme === "dark" ? "#FFFEFF" : "black",
+          activeTintColor: '#34AF4A',
+          inactiveTintColor: colorScheme === 'dark' ? '#FFFEFF' : 'black',
         }}
       >
         <Tab.Screen name="Movimientos" component={Movements} />
         <Tab.Screen name="Estadisticas" component={Stats} />
-        <Tab.Screen name="Cuentas" component={Accounts} />
+        <Tab.Screen name="Cuentas" component={AccountNavigator} />
         <Tab.Screen name="Ajustes" component={Settings} />
       </Tab.Navigator>
     );
