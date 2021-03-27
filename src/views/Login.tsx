e82baad0ee4bb24185a44977c823e0ee6a82a4f3
David Mendoza <d.mendoza99@unitec.edu>
2021-03-27 17:38:47 -0600

    validaciones necesarias

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,20 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, KeyboardAvoidingView } from 'react-native';
 import { Text, Button, Input, withTheme } from 'react-native-elements';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { RootStateOrAny, useDispatch } from 'react-redux';
-import { LOGIN } from '../redux/actions';
+import { useDispatch } from 'react-redux';
+import validator from 'validator';
+import { LOGIN } from '../redux/thunks/auth';
 
 // @ts-ignore
 const Login = ({ navigation }) => {
   const dispatch = useDispatch();
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  useEffect(() => {
+    setDisabled(
+      validator.isEmpty(email) ||
+        shouldShowError('email') ||
+        shouldShowError('password')
+    );
+  }, [email, password]);
+
+  const shouldShowError = (variable: string) => {
+    switch (variable) {
+      case 'email': {
+        return !validator.isEmpty(email) && !validator.isEmail(email);
+      }
+      case 'password': {
+        return validator.isEmpty(password);
+      }
+    }
+    return false;
+  };
   return (
     <KeyboardAvoidingView behavior="height" style={style.formContainer}>
       <Input
         label="Tu correo"
         placeholder="ejemplo@lempira.com"
+        errorStyle={{ color: '#B34A37' }}
+        errorMessage={
+          (shouldShowError('email') && 'Ingrese un correo valido') || ''
+        }
         leftIcon={{ type: 'ionicon', name: 'mail-outline' }}
         onChangeText={(text) => {
           setEmail(text);
@@ -31,6 +56,7 @@ const Login = ({ navigation }) => {
       />
       <Button
         title="Acceder"
+        disabled={disabled}
         onPress={() => {
           dispatch(LOGIN(email, password));
         }}
--- src/views/Signup.tsx
+++ src/views/Signup.tsx
@@ -1,19 +1,63 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, KeyboardAvoidingView } from 'react-native';
 import { Button, Input, withTheme } from 'react-native-elements';
 import { useDispatch } from 'react-redux';
-import { SIGNUP } from '../redux/actions';
+import { SIGNUP } from '../redux/thunks/auth';
+import validator from 'validator';
 
 const Signup = () => {
   const dispatch = useDispatch();
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    setDisabled(
+      validator.isEmpty(email) ||
+        validator.isEmpty(name) ||
+        validator.isEmpty(password) ||
+        shouldShowError('name') ||
+        shouldShowError('email') ||
+        shouldShowError('password')
+    );
+  }, [name, email, password]);
+
+  const shouldShowError = (variable: string) => {
+    switch (variable) {
+      case 'name': {
+        return (
+          !validator.isEmpty(name) &&
+          !validator.isAlpha(name.replace(/\s/g, ''))
+        );
+      }
+      case 'email': {
+        return !validator.isEmpty(email) && !validator.isEmail(email);
+      }
+      case 'password': {
+        return (
+          !validator.isEmpty(password) &&
+          !validator.isStrongPassword(password, {
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 0,
+          })
+        );
+      }
+    }
+    return true;
+  };
   return (
     <KeyboardAvoidingView behavior="height" style={style.formContainer}>
       <Input
         label="Tu nombre"
         placeholder="John Doe"
+        errorStyle={{ color: '#B34A37' }}
+        errorMessage={
+          (shouldShowError('name') && 'Su nombre no puede tener numeros') || ''
+        }
         leftIcon={{ type: 'ionicon', name: 'person-outline' }}
         value={name}
         onChangeText={(text) => {
@@ -23,8 +67,11 @@ const Signup = () => {
       <Input
         label="Tu correo"
         placeholder="ejemplo@lempira.com"
+        errorStyle={{ color: '#B34A37' }}
+        errorMessage={
+          (shouldShowError('email') && 'Ingrese un correo valido') || ''
+        }
         leftIcon={{ type: 'ionicon', name: 'mail-outline' }}
-        value={email}
         onChangeText={(text) => {
           setEmail(text);
         }}
@@ -33,6 +80,12 @@ const Signup = () => {
         label="Contraseña"
         secureTextEntry
         placeholder="Contraseña"
+        errorStyle={{ color: '#B34A37' }}
+        errorMessage={
+          (shouldShowError('password') &&
+            'Su contraseña debe ser de 8 o mas caracteres, contener minimo una mayuscula una minuscula y un numero') ||
+          ''
+        }
         leftIcon={{ type: 'ionicon', name: 'lock-closed-outline' }}
         value={password}
         onChangeText={(text) => {
@@ -41,6 +94,7 @@ const Signup = () => {
       />
       <Button
         title="Crear Cuenta"
+        disabled={disabled}
         onPress={() => {
           dispatch(SIGNUP(email, password, name));
         }}
