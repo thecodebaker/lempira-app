ac62183f63dede0391b98cb7958f7702adda1012
David Mendoza <d.mendoza99@unitec.edu>
2021-03-27 17:42:25 -0600

    crea cuenta con un alert bonis, esto va cambiar mucho

--- src/views/CreateAccount.tsx
+++ src/views/CreateAccount.tsx
@@ -1,25 +1,37 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
 import { Picker } from '@react-native-picker/picker';
 import { useColorScheme } from 'react-native-appearance';
 import User from '../Types/User';
+import { createAccount } from '../redux/thunks/accounts';
 
-const CreateAccount = () => {
+// @ts-ignore
+const CreateAccount = ({ navigation }) => {
   const dispatch = useDispatch();
   const colorScheme = useColorScheme();
   const user: User = useSelector((state: RootStateOrAny) => state.auth.user);
-  const [currency, setCurrency] = useState('');
+  const [currency, setCurrency] = useState('HNL');
+  const [name, setName] = useState('');
+  const [ammount, setAmmount] = useState('');
   return (
     <View style={style.mainContainer}>
-      <Text h1>Crear</Text>
       <Input
+        value={name}
+        onChangeText={(text) => {
+          setName(text);
+        }}
         placeholder="Nombre Eje: Efectivo, BAC"
         leftIcon={{ type: 'ionicon', name: 'bookmark-outline' }}
       />
       <Input
         placeholder="Valor inicial"
+        value={`${ammount}`}
+        onChangeText={(text) => {
+          setAmmount(text);
+        }}
+        keyboardType="number-pad"
         leftIcon={{ type: 'ionicon', name: 'cash-outline' }}
       />
       <Picker
@@ -41,10 +53,33 @@ const CreateAccount = () => {
       </Picker>
       <Button
         title="Crear Cuenta"
+        onPress={() => {
+          dispatch(
+            createAccount(
+              user.token,
+              name,
+              currency,
+              Math.abs(Number(ammount) || 0),
+              (Number(ammount) || 0) >= 0
+            )
+          );
+          Alert.alert(
+            'Cuenta Creada',
+            `La cuenta ${name} fue creada con exito`,
+            [
+              {
+                text: 'OK',
+                onPress: () => {
+                  navigation.goBack();
+                },
+              },
+            ]
+          );
+        }}
         icon={{
           type: 'ionicon',
           name: 'add',
-          color: colorScheme === 'dark' ? 'white' : 'gray',
+          color: 'white',
         }}
       />
     </View>
