c61f930c26d1f11f58585dd5bc8b41170ab05af1
David Mendoza <d.mendoza99@unitec.edu>
2021-04-13 19:30:13 -0600

    empty list por fin programado

---
+++ src/components/EmptyList.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Icon } from 'react-native-elements';
+
+type propType = {
+  texto: string;
+};
+const EmptyList = ({ texto }: propType) => {
+  return (
+    <View style={styles.noDataContainer}>
+      <View style={{ flex: 1 }}>
+        <Icon size={64} type="material-community" name="emoticon-sad-outline" />
+      </View>
+      <View style={{ flex: 3 }}>
+        <Text h4>{texto}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  noDataContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+});
+export default EmptyList;
